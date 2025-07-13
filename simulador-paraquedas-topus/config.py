1b80a6a37c64c8a2d353413da091a3170191d483
matncb <matheus.brustelo@gmail.com>
2025-07-12 22:07:51 -0300

    Simulador

---
+++ simulador-paraquedas-topus/config.py
@@ -0,0 +1,22 @@
+#config.py
+#Arquivo de Configurações do simulador
+
+#Configurações do Foguete/Paraquedas
+#Tudo no SI
+
+MASSA = 5 
+APOGEU = 20650 
+ALTURA_ABERTURA_DROGUE = 10000 
+ALTURA_ABERTURA_REEFING = 1000 
+ALTURA_ABERTURA_MAIN = 500 
+
+#Parâmetros para calcular arrasto
+
+AREA_DROGUE = 0.3
+CD_DROGUE = 0.75
+AREA_REEFING = 1
+CD_REEFING = 0.75
+AREA_MAIN = 3
+CD_MAIN = 0.75
+
+     
